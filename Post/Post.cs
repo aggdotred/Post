5a6fbce77f854adc5b4f0e81680aba92d1baa072
Adam Gutierrez <adam.gutierrez@hyland.com>
2021-01-21 12:28:41 -0600

    Add project files.

---
+++ Post/Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Post
+{
+        class Post {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; }
+        int UpVotes;
+        int DownVotes;
+
+        public Post(string _title, string _description)
+        {
+            Title = _title;
+            Description = _description;
+            UpVotes = 0;
+            DownVotes = 0;
+            CreatedAt = DateTime.Now;
+        }
+
+        public void voteUp()
+        {
+            UpVotes++;
+        }
+
+        public void voteDown()
+        {
+            DownVotes++;
+        }
+
+        public int getVotes()
+        {
+            return UpVotes - DownVotes;
+        }
+    }
+}
---
+++ Post/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post = new Post("test", "this is only a test");
+            post.voteUp();
+            post.voteUp();
+            post.voteUp();
+            post.voteDown();
+
+            Console.WriteLine(post.Title);
+            Console.WriteLine(post.CreatedAt);
+            Console.WriteLine(post.getVotes());
+        }
+    }
+}
